858ec44912c8e6cffd7ece3731d58f3c87331068
Antonio <your_email@example.com>
2021-05-17 14:00:43 +0800

    Add: proxy and timeout

--- cli.js
+++ cli.js
@@ -22,6 +22,8 @@ program
     .option('-u, --auth-username <user>', 'Basic authentication username.')
     .option('-p, --auth-password <password>', 'Basic authentication password.')
     .option('-d, --debug', 'Enable logging of debugging information.')
+    .option('-t, --timeout', 'Set timeout time.')
+    .option('-proxy, --proxy', 'Set proxy.')
     .action((config) => {
         if (config.debug) {
             logger.level = 5;
